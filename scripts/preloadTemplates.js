3dd572d8eea3739db376d02c4f6175057c4f6bb6
barrlo <dist.runner3@gmail.com>
2024-11-18 17:04:12 -0600

    start making a gifts tab

--- scripts/preloadTemplates.js
+++ scripts/preloadTemplates.js
@@ -6,9 +6,9 @@ export const preloadHandlebarsTemplates = async function () {
         //Sheet tabs
         'modules/foundry-wwn-godbound-sheet/scripts/templates/actors/partials/character-header.html',
         'modules/foundry-wwn-godbound-sheet/scripts/templates/actors/partials/character-attributes-tab.html',
-        'modules/foundry-wwn-godbound-sheet/scripts/templates/actors/partials/character-spells-tab.html',
+        'modules/foundry-wwn-godbound-sheet/scripts/templates/actors/partials/character-gifts-tab.html',
         'modules/foundry-wwn-godbound-sheet/scripts/templates/actors/partials/character-inventory-tab.html',
-        'modules/foundry-wwn-godbound-sheet/scripts/templates/actors/partials/character-projects.hbs',
+        'modules/foundry-wwn-godbound-sheet/scripts/templates/actors/partials/character-projects-tab.hbs',
         'modules/foundry-wwn-godbound-sheet/scripts/templates/actors/partials/character-notes-tab.html'
     ];
     return loadTemplates(templatePaths);
