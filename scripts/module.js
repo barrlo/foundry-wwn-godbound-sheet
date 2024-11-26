463dafb9800737e5ecf2ee1228d69efa4141728f
barrlo <58633227+barrlo@users.noreply.github.com>
2024-11-25 22:55:40 -0600

    better control over items (#3)
    * copy over stuff for more control over items
    
    * remove skill and shock from weapon rolls

--- scripts/module.js
+++ scripts/module.js
@@ -1,11 +1,25 @@
 import {BarrloActorSheetCharacter} from './actor/character-sheet.js';
+import {BarrloActor} from './actor/entity.js';
+import {BarrloItemSheet} from './item/item-sheet.js';
+import {BarrloItem} from './item/entity.js';
 import {preloadHandlebarsTemplates} from './preloadTemplates.js';
+import {registerSettings} from './settings.js';
 
 Hooks.once('init', async function () {
+    // Register custom system settings
+    registerSettings();
+
+    CONFIG.Actor.documentClass = BarrloActor;
+    CONFIG.Item.documentClass = BarrloItem;
+
     Actors.registerSheet('wwn', BarrloActorSheetCharacter, {
         types: ['character'],
         makeDefault: false,
         label: 'barrlo.GodboundCharacterSheet'
+    });
+    Items.registerSheet('wwn', BarrloItemSheet, {
+        makeDefault: true,
+        label: 'barrlo.GodboundItemSheet'
     });
 
     await preloadHandlebarsTemplates();
